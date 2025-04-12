c47274639d8a50ae7123fed48207df574612d9eb
sprubh <141857992+sprubh@users.noreply.github.com>
2025-04-12 19:49:05 -0400

    Update gradebook.js=

--- gradebook.js
+++ gradebook.js
@@ -1,1 +1,10 @@
+function fetchGradeData() {
+  console.log("Fetching grade data...")
+}
 
+function populateGradebook(data) {
+  console.log("Populating gradebook with data:",data);
+}
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+//END REMOVE
