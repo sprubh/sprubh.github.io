aab39e7e4f07e0fdd835185e1db11c63e33a0250
sprubh <141857992+sprubh@users.noreply.github.com>
2025-05-02 22:30:12 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -1,10 +1,35 @@
 function fetchGradeData() {
-  console.log("Fetching grade data...")
-}
+        console.log("Fetching grade data...");
+        let xhr = new XMLHttpRequest();
+        let apiRoute = "/api/grades";
+        xhr.onreadystatechange = function() {
+          let results;
+          if(xhr.readyState === xhr.DONE) {
+            if(xhr.status !=200) {
+              console.error('could not get grades. Status: ${xhr.status}')
+              populateGradebook(JSON.parse(xhr.responseText));
+      
+      }
+        }
+        }.bind(this);
+      xhr.open("get",apiRoute, true);
+      xhr.send();
+      }
 //
 function populateGradebook(data) {
   console.log("Populating gradebook with data:",data);
-}
+  let tableElm = document.getElementaryById("gradebook");
+    data.forEach(function(assignment){
+      let row = document.createElement("tr");
+      let columns = [];
+      columns.name = document.createElement('td');
+      columns.name.appendChild (
+            document.createTextNode(assignment.last_name + ", " + assignment.first_name)
+        );
+      row.appendChild(columns.name);
+      row.appendChild(columns.grade);
+      tableElm.appendChild(row);
+});
 const gradeData = fetchGradeData();
 populateGradebook(gradeData);
 //END REMOVE
